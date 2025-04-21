e142132a727c72875c3442aa779f0a8dcd6b1cb4
Emre Akın <emreakinn60@gmail.com>
2025-04-21 21:55:27 +0300

    içerik ekleme modulü yapıldı

---
+++ admin/yazilar.php
@@ -0,0 +1,28 @@
+<?php require_once('header.php'); ?>
+
+
+<!-- Admin Body Section Start -->
+<div class="row">
+    <div class="col-md-6">
+        <h3>Yazılar</h3>
+    </div>
+    <div class="col-md-6 text-end">
+        <a href="yazi-ekle.php" class="btn btn-info text-white">Yeni yazı ekle</a>
+    </div>
+</div>
+<div class="row">
+    <div class="-col-12">
+        <table class="table table-striped mt-3">
+            <tr>
+                <th>Başık 1</th>
+                <th>Başık 2</th>
+                <th>Başık 3</th>
+                <th>Başık 4</th>
+                <th>Başık 5</th>
+                <th>Başık 6</th>
+            </tr>
+        </table>
+    </div>
+</div>
+<!-- Admin Body Section End -->
+<?php require_once('footer.php'); ?>
